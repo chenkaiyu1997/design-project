3b428844c22f55b510adcd2e3e8bf6101a98e3dd
chenkaiyu1997 <justryit8@gmail.com>
2017-10-23 17:14:52 +0800

    try to fix

--- src/Mobile/Homepage.js
+++ src/Mobile/Homepage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-
+import ReactPlayer from 'react-player'
 export default class extends Component {
   constructor() {
     super()
@@ -14,8 +14,9 @@ export default class extends Component {
           width: '100%',
           height: '100%' ,
           overflow: 'hidden'}}>
-
-        <video controls autoPlay muted loop playsInline poster="/image/verti_poster.jpg" style={{
+        {
+          false ? 
+          <img src="image/vertical.gif" style={{
             minWidth: '100%',
             minHeight: '100%',
             width: 'auto',
@@ -23,9 +24,18 @@ export default class extends Component {
             position: 'absolute',
             top: '50%',
             left: '50%',
-            transform: 'translate(-50%,-50%)'}}>
-          <source src="/image/vertical.mp4" type="video/mp4" />
-        </video>
+            transform: 'translate(-50%,-50%)'}}/>
+          :
+          <ReactPlayer url='/image/vertical.mp4' playing controls playsinline loop muted style={{
+              minWidth: '100%',
+              minHeight: '100%',
+              width: 'auto',
+              height: 'auto',
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%,-50%)'}}/>
+        }
       </div>
     )
   }
